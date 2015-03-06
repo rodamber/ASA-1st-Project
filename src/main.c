a732f596bda8d77e4e026bc601cfb990bd1eac13
rodamber <rodrigoambernardo@gmail.com>
2015-03-06 14:58:25 +0000

    sair do quarto

--- src/main.c
+++ src/main.c
@@ -3,11 +3,38 @@
 #include <time.h>
 
 void
-print_array(int *v, int size, char c)
+print_array_h(const int *a, size_t nmemb)
 {
-    for (int i = 0; i < size; i++) {
-        printf("%c[%d]=%d\n", c, i, v[i]);
+    for (size_t i = 0; i < nmemb - 1; i++) {
+        printf("+---");
     }
+    printf("+---+\n");
+
+    for (size_t i = 0; i < nmemb; i ++) {
+        printf("| %d ", a[i]);
+    }
+    printf("|\n");
+
+    for (size_t i = 0; i < nmemb - 1; i++) {
+        printf("+---");
+    }
+    printf("+---+\n");
+}
+
+void
+print_array_v(const int *a, size_t nmemb)
+{
+    printf("+---+\n");
+    for (size_t i = 0; i < nmemb; i++) {
+        printf("| %d |\n", a[i]);
+        printf("+---+\n");
+    }
+}
+
+void
+print_int(const void * p)
+{
+    printf("%d", *((int*) p));
 }
 
 int
@@ -17,16 +44,16 @@ random_mm (const int min, const int max)
 }
 
 void
-csort(int v[], const int min, const int max, const int l, const int r)
+csort(int *a, const int min, const int max, const int l, const int r)
 {
     const int csize = max - min + 1;
-    const int vsize = r - l + 1;
+    const int asize = r - l + 1;
 
     int *count = calloc(csize, sizeof (int));
-    int *buf   = malloc(vsize * sizeof (int));
+    int *buf   = malloc(asize * sizeof (int));
 
     for (int i = l; i <= r; i++) {
-        count[v[i] - min]++;
+        count[a[i] - min]++;
     }
 
     for (int i = 1; i < csize; i++) {
@@ -34,37 +61,45 @@ csort(int v[], const int min, const int max, const int l, const int r)
     }
 
     for (int i = r; i >= l; i--) {
-        buf[count[v[i] - min] - 1] = v[i];
-        --count[v[i] - min];
+        buf[count[a[i] - min] - 1] = a[i];
+        --count[a[i] - min];
     }
+    free(count);
 
     for (int i = r; i >= l; i--) {
-        v[i] = buf[i - l];
+        a[i] = buf[i - l];
     }
-
-    free(count);
     free(buf);
 }
 
-#define SIZE  8
-#define MIN 0
-#define MAX 5
+#define SIZE 21
+#define MIN  0
+#define MAX  5
 
 int
 main(void)
 {
-    int A[SIZE];
+    int a[SIZE];
 
     srand(time(NULL));
     for (int i = 0; i < SIZE; i++) {
-        A[i] = random_mm(MIN, MAX);
+        a[i] = random_mm(MIN, MAX);
     }
 
-    puts("=============");
-    puts("Counting Sort");
-    puts("=============");
-    csort(A, MIN, MAX, 0, SIZE-1);
-    print_array(A, SIZE, 'A');
+    char *padding = calloc(1 + SIZE / 2, sizeof (char));
+
+    int i = 0;
+    while (padding[i++] = ' ')
+        ;
+
+    printf("%s=============", padding);
+    printf("%sCounting Sort", padding);
+    printf("%s=============", padding);
+
+    csort(a, MIN, MAX, 0, SIZE-1);
+
+    print_array_h(a, SIZE);
+    //print_array_v(a, SIZE);
 
     return 0;
 }
